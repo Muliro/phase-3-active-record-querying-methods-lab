8de8d7765d98cc45773d196963d4361da1d7e9d2
michael muliro <michael.muliro@student.moringaschool.com>
2022-08-26 18:16:10 +0300

    commit tree 7e9d2b9c252d24602264ffc267d4302de027f2f2

---
+++ db/migrate/20220826150558_code.rb
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+class Code < ActiveRecord::Migration[6.1]
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
+    end
+  end
+end
